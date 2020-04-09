66cd3e935297e9fff6d84fa93519af6b0ecad784
kaw393939 <keith@webizly.com>
2020-04-10 00:54:41 +0200

    Initial Commit

---
+++ neo4j/neo4jProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Neo4j;
+using Neo4j.Driver;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace data
+{
+    public class neo4jProvider : IDisposable
+    {
+        static string _uri = "bolt://" + Settings.host + ":" + Settings.port + "/db/neo4j";
+
+        static IDriver _driver;
+         public  neo4jProvider()
+        {
+
+            _driver = GraphDatabase.Driver(_uri, AuthTokens.Basic(Settings.username, Settings.password));
+
+
+        }
+        public async Task<List<IRecord>> queryExecute(string query)
+        {
+           
+            IAsyncSession session = _driver.AsyncSession();
+            IResultCursor cursor = await session.RunAsync(query);
+            
+            var results = await cursor.ToListAsync();
+            return results;
+         
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
---
+++ neo4j/settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//https://github.com/tonerdo/dotnet-env
+using DotNetEnv;
+
+namespace data
+{
+    public static class Settings
+    {
+        static Settings()
+        {
+            Env.Load();
+        }
+
+        public static string host => Env.GetString("HOST");
+        public static string username => Env.GetString("USERNAME");
+        public static string password => Env.GetString("PASSWORD");
+        public static string port => Env.GetString("PORT");
+        public static string database => Env.GetString("database");
+
+
+
+
+    }
+}
